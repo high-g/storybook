07bbec611db8e6228484efebc561f08fa3eae7f1
fumiya <altema1217bbfumi@live.jp>
2021-09-22 10:56:49 +0900

    feat: Task.js, Task.stories.jsno

---
+++ src/components/Task.js
@@ -0,0 +1,9 @@
+const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
+  return (
+    <div className="list-item">
+      <input type="text" value={title} readOnly />
+    </div>
+  )
+}
+
+export default Task
