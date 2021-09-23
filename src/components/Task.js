85a247bec22751b615762efd8fa2b3ea77fef08a
fumiya <altema1217bbfumi@live.jp>
2021-09-23 19:24:15 +0900

    feat: storybookへストーリー追加

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,14 @@
 const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
   return (
-    <div className="list-item">
-      <input type="text" value={title} readOnly />
+    <div className={`list-item ${state}`}>
+      <label classname="checkbox">
+        <input type="checkbox" defaultChecked={state === 'TASK_ARCHIVED'} disabled name="checked" />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)}></span>
+      </label>
+      <div className="title">
+        <input type="text" value={title} readOnly placeholder="Input title" />
+      </div>
+      <div className="actions" onClick={(event) => event.stopPropagation()}></div>
     </div>
   )
 }
