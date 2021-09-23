e6859b61f772dadd31679ad448916b95512af8ce
fumiya <altema1217bbfumi@live.jp>
2021-09-23 19:29:45 +0900

    feat: aタグ追加

--- src/components/Task.js
+++ src/components/Task.js
@@ -8,7 +8,14 @@ const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
       <div className="title">
         <input type="text" value={title} readOnly placeholder="Input title" />
       </div>
-      <div className="actions" onClick={(event) => event.stopPropagation()}></div>
+      <div className="actions" onClick={(event) => event.stopPropagation()}>
+        {state !== 'TASK_ARCHIVED' && (
+          // eslint-disable-next-line jsx-a11y/anchor-is-valid
+          <a onClick={() => onPinTask(id)}>
+            <span className={`icon-star`} />
+          </a>
+        )}
+      </div>
     </div>
   )
 }
